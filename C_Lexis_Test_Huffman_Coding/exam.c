94424732af107181eb68434cd21bacba2bcb7da9
Ashly Lau <al5217@point10.doc.ic.ac.uk>
2018-06-08 23:02:20 +0100

    completed questions. to debug.

--- C_Lexis_Test_Huffman_Coding/exam.c
+++ C_Lexis_Test_Huffman_Coding/exam.c
@@ -120,15 +120,36 @@ void huffman_tree_list_free(huffman_tree_list_t *l) {
  * Returns 1 if the string s contains the character c and 0 if it does not.
  */
 int contains(char *s, char c) {
+  if (s == NULL) {
+    perror("contains");
+    exit(EXIT_FAILURE);
+  }
+  for (int i = 0; i < strlen(s); i++) {
+    if (c == s[i]) {
+      return 1;
+    }
+  }
   return 0;
 }
+//works
 
 /*
  * Returns the number of occurrences of c in s.
  */
 int frequency(char *s, char c) {
-  return 0;
+  if (s == NULL) {
+    perror("frequency");
+    exit(EXIT_FAILURE);
+  }
+  int count = 0;
+  for (int i = 0; i < strlen(s); i++) {
+    if (c == s[i]) {
+      count++;
+    }
+  }
+  return count;
 }
+//worksss
 
 /*
  * Takes a string s and returns a new heap-allocated string containing only the
@@ -138,8 +159,25 @@ int frequency(char *s, char c) {
  *      characters.
  */
 char *nub(char *s) {
-  return NULL;
+  if (s == NULL) {
+    perror("nub");
+    exit(EXIT_FAILURE);
+  }
+  char *result = malloc(MAX_STRING_LENGTH - 1);
+  if (result == NULL) {
+    perror("nub");
+    exit(EXIT_FAILURE);
+  }
+  int index = 0;
+  for (int i = 0; i < strlen(s); i++) {
+    if (contains(result, s[i]) == 0) { //char is not added to result yet
+      result[strlen(result)] = s[i];
+    }
+    index++;
+  }
+  return result;
 }
+//worksssss
 
 /*
  * Adds the Huffman tree t to the list l, returning a pointer to the new list.
@@ -152,8 +190,35 @@ char *nub(char *s) {
  */
 huffman_tree_list_t *huffman_tree_list_add(huffman_tree_list_t *l,
                                             huffman_tree_t *t) {
-
-  return NULL;
+  if (l == NULL || t == NULL) {
+    perror("add");
+    exit(EXIT_FAILURE);
+  }
+  huffman_tree_t *thisTree = malloc(sizeof(*thisTree));
+  thisTree = l->tree;
+  huffman_tree_list_t *thisList = malloc(sizeof(*thisList));
+  thisList = l;
+  huffman_tree_list_t *newList = malloc(sizeof(*newList));
+  if (thisTree->count > t->count) { //add to head of list
+    newList->tree = t;
+    newList->next = l;
+    return newList;
+  }
+  else {
+    huffman_tree_list_t *nextList = malloc(sizeof(*nextList));
+    nextList = l->next;
+    huffman_tree_t *nextTree = malloc(sizeof(*nextTree));
+    nextTree = l->next->tree;
+    while (nextTree->count < t->count || nextList == NULL) {
+      thisList = nextList;
+      nextList = nextList->next;
+      nextTree = nextList->tree;
+    }
+    newList->tree = t;
+    newList->next = nextList;
+    thisList->next = newList;
+    return l;
+  }
 }
 
 /*
@@ -167,7 +232,37 @@ huffman_tree_list_t *huffman_tree_list_add(huffman_tree_list_t *l,
  *        trees it contains.
  */
 huffman_tree_list_t *huffman_tree_list_build(char *s, char *t) {
-  return NULL;
+  if (s == NULL || t == NULL) {
+    perror("list_build");
+    exit(EXIT_FAILURE);
+  }
+
+  //create first tree and initiate first list;
+  huffman_tree_list_t *list = malloc(sizeof(*list));
+  char letter = t[0];
+  int freq = frequency(s, letter);
+  huffman_tree_t *firstTree = malloc(sizeof(*firstTree));
+  firstTree->count = freq;
+  firstTree->letter = letter;
+  firstTree->left = NULL;
+  firstTree->right = NULL;
+  list->tree = firstTree;
+  list->next = NULL;
+
+  //iterate through to create the rest of trees and add them to the list
+  for (int i = 1; i < strlen(t); i++) {
+    char letter = t[i];
+    int freq = frequency(s, letter);
+    huffman_tree_t *newTree = malloc(sizeof(*newTree));
+    newTree->count = freq;
+    newTree->letter = letter;
+    newTree->left = NULL;
+    newTree->right = NULL;
+
+    huffman_tree_list_add(list, newTree);
+  }
+
+  return list;
 }
 
 /*
@@ -179,7 +274,27 @@ huffman_tree_list_t *huffman_tree_list_build(char *s, char *t) {
  * Post:  The resuling list contains a single, correctly-formed Huffman tree.
  */
 huffman_tree_list_t *huffman_tree_list_reduce(huffman_tree_list_t *l) {
-  return NULL;
+  if (l == NULL) {
+    perror("list_reduce");
+    exit(EXIT_FAILURE);
+  }
+
+  while (l->next != NULL) {
+    huffman_tree_t *firstT = malloc(sizeof(*firstT));
+    firstT = l->tree;
+    huffman_tree_t *secondT = malloc(sizeof(*secondT));
+    secondT = l->next->tree;
+    huffman_tree_list_free(l);
+    huffman_tree_list_free(l);
+
+    huffman_tree_t *newTree = malloc(sizeof(*newTree));
+    newTree->count = firstT->count + secondT->count;
+    newTree->left = firstT;
+    newTree->right = secondT;
+
+    huffman_tree_list_add(l, newTree);
+  }
+  return l;
 }
 
 /*
@@ -201,3 +316,14 @@ char *huffman_tree_encode(huffman_tree_t *t, char *s) {
 char *huffman_tree_decode(huffman_tree_t *t, char *code) {
   return NULL;
 }
+//
+//
+//
+// int main(void) {
+//   printf("contains test: %d\n", contains("hello", 'f'));
+//   printf("freq test: %d\n", frequency("banana", 't'));
+//   char *nubb = malloc(MAX_STRING_LENGTH-1);
+//   memcpy(nubb, nub("mississiprerepi"), 10);
+//   printf("nub test: %s\n", nubb);
+//
+// }
